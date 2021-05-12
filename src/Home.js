2b5583bf5163e010df65f80d85851572ea055079
ahmetfarukyilmaz <farukylm34@gmail.com>
2021-05-12 23:54:38 +0300

    useState,props learned

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
-const Home = () => {
-    return ( 
+import { useState } from "react";
+import BlogList from "./BlogList";
 
-        <div className="home">
-            <h2>HomePage</h2>
-        </div>
-     );
-}
- 
+const Home = () => {
+    const [blogs,setBlogs]=useState([
+        
+        {title:"Blog 1",body:"test1",author:"faruk",id:1},
+        {title:"Blog 2",body:"test2",author:"faruk",id:2},
+        {title:"Blog 3",body:"test3",author:"faruk",id:3},
+    ])
+  return (
+    <div className="home">
+        <BlogList blogs={blogs} title="Blog Master"/>
+        <BlogList blogs={blogs.filter((blog)=> blog.id==1 )} title="Blog Master"/>
+    
+    </div>
+  );
+};
+
 export default Home;
