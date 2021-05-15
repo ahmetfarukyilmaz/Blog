66548c198ac18a66ae89214661e814f79fc331a8
ahmetfarukyilmaz <farukylm34@gmail.com>
2021-05-15 20:31:55 +0300

    Route parameters,Router Links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
-              <Home/>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails/>
             </Route>
           </Switch>
         </div>
